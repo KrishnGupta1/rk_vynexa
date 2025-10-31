7525975493ed670157999d6f4378d00a8576c09d
KrishnGupta1 <guptakrishn258@gmail.com>
2025-10-31 12:00:16 +0530

    Add showMessage function to display success message

--- public/app.js
+++ public/app.js
@@ -1,1 +1,6 @@
-console.log("RK_VYNEXA site running successfully!");
+function showMessage() {
+  const msg = document.getElementById('msg');
+  msg.innerText = "🎉 Firebase Auto Deploy is 100% Working!";
+  msg.style.color = "#00ffae";
+  msg.style.fontSize = "20px";
+}
